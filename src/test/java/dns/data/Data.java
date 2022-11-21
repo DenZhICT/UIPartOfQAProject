bf3ad4ecae091964505cfa45cdf2fc6499ca2682
Журавлёв Денис <deniszhict@gmail.com>
2022-11-21 20:03:31 +0300

    add data

---
+++ src/test/java/dns/data/Data.java
@@ -0,0 +1,30 @@
+package dns.data;
+
+public class Data {
+
+    private final String city = "Уфа",
+    tech = "Ноутбуки",
+    mainCatalog="Отдых и развлечения",
+    catalog="Развлечения и хобби",
+    subCatalog="Настольные игры";
+    public String getCity() {
+        return city;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public String getMainCatalog() {
+        return mainCatalog;
+    }
+
+    public String getCatalog() {
+        return catalog;
+    }
+
+    public String getSubCatalog() {
+        return subCatalog;
+    }
+
+}
